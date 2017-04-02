2830b97474f6e120b9e2dd3c7e1f21dd2ef99339
Jakub Kania <kubak.15@gmail.com>
2017-04-02 19:19:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,3 +12,24 @@ document.querySelector(".nav-bar__nav").addEventListener('click', function(e){
 document.addEventListener("click", function (){
   document.querySelector(".nav-bar").classList.remove('nav-bar_open');
 }, false);
+
+
+
+
+
+$(document).ready(function() {
+  $('#slides').superslides({
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true,
+    hashchange: true,
+    scrollable: true,
+    animation: 'slide',
+    play: 2500
+  });
+});
+
+
+
+
+
